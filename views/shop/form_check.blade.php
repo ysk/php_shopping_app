d20b5c48c1bdb40091e159fa31d62a5c07a23944
Yusuke Ikeda <yusuke_aono@yahoo.co.jp>
2021-02-15 17:07:44 +0900

    修正

--- views/shop/form_check.blade.php
+++ views/shop/form_check.blade.php
@@ -1,8 +1,7 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'] . '../lib/common/setting.php');
-require_once($_DIR . '../lib/common/session_member.php');
-require_once($_DIR . '../lib/common/functions.php');
-require_once($_DIR . '../lib/shop/shop_form_check.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '../lib/common/session_member.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '../lib/common/functions.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '../lib/shop/shop_form_check.php');
 ?>
 @extends('layouts.site_frame')
 
